bf00d0455d0ddb0114c3fb245315426114e32e3b
GotohFumiaki <fgotoh@smile-lab.com>
2019-03-08 16:54:51 +0900

    1st commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var table = document.getElementById("test_table");
+(function(){
+    var table = document.getElementById("test_table");
+    // var row = table.insertRow(-1);
+    // var cell = rows[0].insertCell(-1);
+}());
