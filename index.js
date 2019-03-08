7d094b01b021831e4d003bb5be7db97f0df7e00e
GotohFumiaki <fgotoh@smile-lab.com>
2019-03-08 20:20:30 +0900

    Javascriptを読み込まない現象の解消

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 (function(){
     var table = document.getElementById("test_table");
-    /*var collection = table.rows;
-    var tr = table.rows[0];
-    var td = tr.cells[0];*/
     var newtr = table.insertRow();
-    for(var i = 0; i < 2; ++i){
+    for(var i = 0; i < 3; ++i){
         var newtd = newtr.insertCell();
         newtd.appendChild( document.createTextNode('testtest'+i) );
     }
