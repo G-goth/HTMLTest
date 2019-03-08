246d3d64b95d5b01474fee35c55d92fb49d3189e
GotohFumiaki <fgotoh@smile-lab.com>
2019-03-08 19:04:05 +0900

    テーブルのセルを増やすための雛形を実装

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var table = document.getElementById("test_table");
 (function(){
     var table = document.getElementById("test_table");
-    // var row = table.insertRow(-1);
-    // var cell = rows[0].insertCell(-1);
+    /*var collection = table.rows;
+    var tr = table.rows[0];
+    var td = tr.cells[0];*/
+    var newtr = table.insertRow();
+    for(var i = 0; i < 2; ++i){
+        var newtd = newtr.insertCell();
+        newtd.appendChild( document.createTextNode('testtest'+i) );
+    }
 }());
