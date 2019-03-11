d4c2bf64683c5e3b2c031372b6fcd23b42737cad
GotohFumiaki <fgotoh@smile-lab.com>
2019-03-11 17:10:10 +0900

    eslint、tsd、typingsの再導入

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/// <reference path="typings/tsd.d.ts" />
+// <reference path="typings/tsd.d.ts" />
 (function () {
     let table = document.getElementById("test_table");
     let newtr = table.insertRow();
