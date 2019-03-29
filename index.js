e11ac8a2086d5a834fc25ae8968eb29b7e29beb0
GotohFumiaki <fgotoh@smile-lab.com>
2019-03-29 20:06:18 +0900

    連番画像の表示

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 (function () {
     let table = document.getElementById("test_table");
     // 行を行末に追加
-    const rowsCount = 20;
+    const rowsCount = 6;
     let rowsArray = new Array();
     for (let i = 0; i < rowsCount; ++i) {
         rowsArray[i] = table.insertRow(-1);
@@ -10,11 +10,11 @@
 
     // セルの挿入
     let hoge = [];
-    for (let i = 0; i < rowsCount; ++i) {
+    for (let i = 1; i < rowsCount; ++i) {
         hoge[i] = [];
         for (let j = 0; j < 3; ++j) {
             hoge[i][j] = rowsArray[i].insertCell(-1);
-            hoge[i][j].innerHTML = '<img src="img1.PNG" height="160" width="120">';
+            hoge[i][j].innerHTML = '<img src="img' + i + '.PNG" height="160" width="120">';
             if(j == 1)
             {
                 hoge[i][j].innerHTML = "Test" + i;
